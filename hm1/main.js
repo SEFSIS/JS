0fa709c4ab7511c663eabc684796089baedbd61c
SEFSIS <sofiagryn@icloud.com>
2022-11-01 17:32:47 +0200

    Add homework JS 1

---
+++ hm1/main.js
@@ -0,0 +1,32 @@
+let h ="'hello'";
+let o ="'owu'";
+let m ="'com'";
+let e ="'ua'";
+let w =1;
+let o2 =10;
+let r =-999;
+let k =123;
+const PI = 3.14;
+let num=2.7;
+let num1=16;
+let t= true;
+let f = false;
+console.log(h,o,m,e,w,o2,r,k,PI,num,num1,t,f);
+//ex2
+    let firstName='Sofia ';
+    let middleName='Mykolayivna ';
+    let lastName='Hryn';
+let person= firstName + middleName + lastName;
+console.log(person);
+//ex3
+let a = 100;
+let b ='100';
+let c = true;
+console. log (typeof a, typeof b, typeof c);
+// Додаткове
+let name = prompt('What is your name?', 'Sofia');
+alert('Your name is ' + name);
+let middle_name = prompt('What is your middle name?', 'Mykolayivna');
+alert('Your middle name is ' + middle_name);
+let age = prompt('How old are you?',19) ;
+alert('You are ' + age+' years old');
